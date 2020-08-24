bac476d7b72ab2a0c78f838db670b19bebc9914c
Gray Strider <zhoga.ivan@gmail.com>
2020-08-24 12:55:17 +1100

    done: if

--- exercises/if/if2.rs
+++ exercises/if/if2.rs
@@ -4,13 +4,11 @@
 // Step 2: Get the bar_for_fuzz and default_to_baz tests passing!
 // Execute the command `rustlings hint if2` if you want a hint :)
 
-// I AM NOT DONE
-
 pub fn fizz_if_foo(fizzish: &str) -> &str {
-    if fizzish == "fizz" {
-        "foo"
-    } else {
-        1
+    match fizzish {
+        "fizz" => "foo",
+        "fuzz" => "bar",
+        _ => "baz"
     }
 }
 
@@ -18,17 +16,17 @@ pub fn fizz_if_foo(fizzish: &str) -> &str {
 #[cfg(test)]
 mod tests {
     use super::*;
-
+    
     #[test]
     fn foo_for_fizz() {
         assert_eq!(fizz_if_foo("fizz"), "foo")
     }
-
+    
     #[test]
     fn bar_for_fuzz() {
         assert_eq!(fizz_if_foo("fuzz"), "bar")
     }
-
+    
     #[test]
     fn default_to_baz() {
         assert_eq!(fizz_if_foo("literally anything"), "baz")
